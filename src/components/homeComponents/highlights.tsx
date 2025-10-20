973b634bd16dc6a17a859c496e6381b8cb31c5fc
maryanne <kaffermaryanne@gmail.com>
2025-10-19 22:22:19 -0300

    .

---
+++ src/components/homeComponents/highlights.tsx
@@ -0,0 +1,38 @@
+import { useLanguage } from "@/context/languageContext";
+import { PostsData } from "../post"
+import { FaAngleDown } from "react-icons/fa";
+
+export default function Highlights() {
+    const posts = PostsData.filter(post => post.highlighted);
+    const best = posts[0]
+    const side = posts.slice(1);
+
+    const { language } = useLanguage()
+
+    return (
+        <section className="h-[100dvh] w-full py-16">
+            <div className="flex gap-7 w-full h-full">
+                <div className="flex relative h-[80dvh] w-full group hover:scale-[1.008] transition-all overflow-hidden">
+                    <img className="absolute w-full h-full object-cover -z-10" src={PostsData[0].squarePicture} />
+                    <div className="flex h-full bg-gradient-to-b from-transparent group-hover:bg-black/60 via-transparent to-black/75 transition-colors duration-400 p-5 cursor-pointer">
+                        <span className="h-[35%] group-hover:h-[100%] flex flex-col gap-5 mt-auto transition-all duration-400">
+                            <h1 className="text-3xl">{best[language].title}</h1>
+                            <h1 className="text-xl group-hover:opacity-0 group-hover:hidden opacity-100 transition-all duration-400">{best[language].resume}</h1>
+                            {(best[language].content).map((content) => (
+                                <h1 className="text-xl group-hover:opacity-100 opacity-0 transition-all duration-600">{content}</h1>
+                            ))}
+                            <span className="flex text-white group-hover:opacity-100 opacity-0 absolute bottom-0 w-full z-10 lg:h-42 h-32 bg-gradient-to-b from-transparent to-black lg:text-5xl text-4xl">
+                                <FaAngleDown className="mx-auto mt-auto" />
+                            </span>
+                        </span>
+                    </div>
+                </div>
+                <div className="flex relative w-full">
+                    {side.map((post) => (
+                        <div></div>
+                    ))}
+                </div>
+            </div>
+        </section>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,19 @@
 import Cover from "@/components/homeComponents/cover";
+import Highlights from "@/components/homeComponents/highlights";
 import Latest from "@/components/homeComponents/latest";
 import DefaultLayout from "@/layouts/default";
 
 export default function IndexPage() {
+  const divider = <span className="w-full h-[2px] bg-black dark:bg-white" />
   return (
     <DefaultLayout>
       <section className="h-full flex flex-col w-full gap-10">
         <Cover />
-        <span className="w-full h-[2px] bg-black dark:bg-white" />
+        {divider}
         <Latest />
-        <span className="w-full h-[2px] bg-black dark:bg-white" />
+        {divider}
+        <Highlights />
+        {divider}
       </section>
     </DefaultLayout>
   );
