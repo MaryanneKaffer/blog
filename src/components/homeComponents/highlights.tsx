e152cd3e6fca486a69d7b8b77c9555140132b17d
maryanne <kaffermaryanne@gmail.com>
2025-10-22 23:23:44 -0300

    .

--- src/components/homeComponents/highlights.tsx
+++ src/components/homeComponents/highlights.tsx
@@ -11,8 +11,8 @@ export default function Highlights() {
 
     return (
         <section className="h-[100dvh] w-full py-16">
-            <div className="flex gap-7 w-full h-full">
-                <div className="flex relative h-[80dvh] w-full group hover:scale-[1.008] transition-all overflow-hidden">
+            <div className="flex gap-8 w-full h-full py-12">
+                <div className="flex relative w-full group hover:scale-[1.008] transition-all overflow-hidden">
                     <img className="absolute w-full h-full object-cover -z-10" src={PostsData[0].squarePicture} />
                     <div className="flex h-full bg-gradient-to-b from-transparent group-hover:bg-black/60 via-transparent to-black/75 transition-colors duration-400 p-5 cursor-pointer">
                         <span className="h-[35%] group-hover:h-[100%] flex flex-col gap-5 mt-auto transition-all duration-400">
@@ -21,15 +21,18 @@ export default function Highlights() {
                             {(best[language].content).map((content) => (
                                 <h1 className="text-xl group-hover:opacity-100 opacity-0 transition-all duration-600">{content}</h1>
                             ))}
-                            <span className="flex text-white group-hover:opacity-100 opacity-0 absolute bottom-0 w-full z-10 lg:h-42 h-32 bg-gradient-to-b from-transparent to-black lg:text-5xl text-4xl">
+                            <span className="flex text-white group-hover:opacity-100 transition-all duration-300 opacity-0 absolute bottom-0 w-full z-10 lg:h-42 h-32 lg:text-5xl text-4xl">
                                 <FaAngleDown className="mx-auto mt-auto" />
                             </span>
                         </span>
                     </div>
                 </div>
-                <div className="flex relative w-full">
-                    {side.map((post) => (
-                        <div></div>
+                <div className="grid relative w-full h-full grid-cols-2 gap-8">
+                    {side.map((post, i) => (
+                        <div key={i} className="cursor-pointer relative group hover:scale-[1.008] transition-all">
+                            <img className="absolute w-full h-full object-cover -z-10" src={post.squarePicture} />
+
+                        </div>
                     ))}
                 </div>
             </div>
--- src/components/post.ts
+++ src/components/post.ts
@@ -13,7 +13,7 @@ export const PostsData = [
         },
         pt: {
             title: "Exemplo 6",
-            resume: "",
+            resume: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque placerat tortor et justo faucibus aliquet. Vivamus luctus est maximus arcu pretium consectetur. Praesent eget purus at ligula imperdiet placerat id ac est. Maecenas eu mi at ante auctor viverra. Sed consectetur ex ligula, vitae euismod sapien iaculis non. Nam eu tincidunt erat. Pellentesque iaculis semper nunc rhoncus viverra. Nullam accumsan venenatis dolor eget facilisis. Donec quis dui in risus suscipit volutpat. Sed sit amet interdum enim.",
             content: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque placerat tortor et justo faucibus aliquet. Vivamus luctus est maximus arcu pretium consectetur. Praesent eget purus at ligula imperdiet placerat id ac est. Maecenas eu mi at ante auctor viverra. Sed consectetur ex ligula, vitae euismod sapien iaculis non. Nam eu tincidunt erat. Pellentesque iaculis semper nunc rhoncus viverra. Nullam accumsan venenatis dolor eget facilisis. Donec quis dui in risus suscipit volutpat. Sed sit amet interdum enim.",
                 "Nunc ultrices gravida metus in pulvinar. Etiam eu scelerisque felis. Nulla erat sapien, molestie id mattis vitae, sagittis eu leo. Duis viverra eget justo eget condimentum. In ut enim consectetur, hendrerit elit at, auctor mi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Curabitur pharetra tortor ornare, ullamcorper arcu quis, tincidunt massa.",
                 "Sed molestie mi ipsum, et lacinia mi aliquet sit amet. Integer pretium a ipsum vitae mollis. Phasellus blandit nisl vel felis fringilla rutrum. Quisque imperdiet lorem elit, et ultricies arcu vehicula fringilla. Maecenas ultrices dolor vitae neque molestie mollis. Cras egestas lorem eu turpis ornare imperdiet. Cras iaculis molestie euismod. Quisque justo leo, pharetra et justo vitae, tempor pulvinar sem. Donec vitae nisl sit amet libero fermentum volutpat. Donec laoreet mauris nec odio lobortis, maximus eleifend arcu mollis. Fusce mauris eros, ullamcorper in dictum at, malesuada nec ipsum. Proin odio ipsum, auctor vitae porttitor lacinia, porttitor eget mi. Maecenas suscipit ut nibh quis imperdiet. Vivamus dictum ligula at gravida tempus. Aenean lacus velit, venenatis in ultrices tempus, tincidunt eu purus."
@@ -40,7 +40,8 @@ export const PostsData = [
                 "Nunc ultrices gravida metus in pulvinar. Etiam eu scelerisque felis. Nulla erat sapien, molestie id mattis vitae, sagittis eu leo. Duis viverra eget justo eget condimentum. In ut enim consectetur, hendrerit elit at, auctor mi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Curabitur pharetra tortor ornare, ullamcorper arcu quis, tincidunt massa.",
                 "Sed molestie mi ipsum, et lacinia mi aliquet sit amet. Integer pretium a ipsum vitae mollis. Phasellus blandit nisl vel felis fringilla rutrum. Quisque imperdiet lorem elit, et ultricies arcu vehicula fringilla. Maecenas ultrices dolor vitae neque molestie mollis. Cras egestas lorem eu turpis ornare imperdiet. Cras iaculis molestie euismod. Quisque justo leo, pharetra et justo vitae, tempor pulvinar sem. Donec vitae nisl sit amet libero fermentum volutpat. Donec laoreet mauris nec odio lobortis, maximus eleifend arcu mollis. Fusce mauris eros, ullamcorper in dictum at, malesuada nec ipsum. Proin odio ipsum, auctor vitae porttitor lacinia, porttitor eget mi. Maecenas suscipit ut nibh quis imperdiet. Vivamus dictum ligula at gravida tempus. Aenean lacus velit, venenatis in ultrices tempus, tincidunt eu purus."
             ],
-        }
+        },
+        highlighted: true
     },
     {
         id: 4,
@@ -61,7 +62,8 @@ export const PostsData = [
                 "Nunc ultrices gravida metus in pulvinar. Etiam eu scelerisque felis. Nulla erat sapien, molestie id mattis vitae, sagittis eu leo. Duis viverra eget justo eget condimentum. In ut enim consectetur, hendrerit elit at, auctor mi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Curabitur pharetra tortor ornare, ullamcorper arcu quis, tincidunt massa.",
                 "Sed molestie mi ipsum, et lacinia mi aliquet sit amet. Integer pretium a ipsum vitae mollis. Phasellus blandit nisl vel felis fringilla rutrum. Quisque imperdiet lorem elit, et ultricies arcu vehicula fringilla. Maecenas ultrices dolor vitae neque molestie mollis. Cras egestas lorem eu turpis ornare imperdiet. Cras iaculis molestie euismod. Quisque justo leo, pharetra et justo vitae, tempor pulvinar sem. Donec vitae nisl sit amet libero fermentum volutpat. Donec laoreet mauris nec odio lobortis, maximus eleifend arcu mollis. Fusce mauris eros, ullamcorper in dictum at, malesuada nec ipsum. Proin odio ipsum, auctor vitae porttitor lacinia, porttitor eget mi. Maecenas suscipit ut nibh quis imperdiet. Vivamus dictum ligula at gravida tempus. Aenean lacus velit, venenatis in ultrices tempus, tincidunt eu purus."
             ],
-        }
+        },
+        highlighted: true
     },
     {
         id: 3,
@@ -103,7 +105,8 @@ export const PostsData = [
                 "Nunc ultrices gravida metus in pulvinar. Etiam eu scelerisque felis. Nulla erat sapien, molestie id mattis vitae, sagittis eu leo. Duis viverra eget justo eget condimentum. In ut enim consectetur, hendrerit elit at, auctor mi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Curabitur pharetra tortor ornare, ullamcorper arcu quis, tincidunt massa.",
                 "Sed molestie mi ipsum, et lacinia mi aliquet sit amet. Integer pretium a ipsum vitae mollis. Phasellus blandit nisl vel felis fringilla rutrum. Quisque imperdiet lorem elit, et ultricies arcu vehicula fringilla. Maecenas ultrices dolor vitae neque molestie mollis. Cras egestas lorem eu turpis ornare imperdiet. Cras iaculis molestie euismod. Quisque justo leo, pharetra et justo vitae, tempor pulvinar sem. Donec vitae nisl sit amet libero fermentum volutpat. Donec laoreet mauris nec odio lobortis, maximus eleifend arcu mollis. Fusce mauris eros, ullamcorper in dictum at, malesuada nec ipsum. Proin odio ipsum, auctor vitae porttitor lacinia, porttitor eget mi. Maecenas suscipit ut nibh quis imperdiet. Vivamus dictum ligula at gravida tempus. Aenean lacus velit, venenatis in ultrices tempus, tincidunt eu purus."
             ],
-        }
+        },
+        highlighted: true
     },
     {
         id: 1,
@@ -122,6 +125,7 @@ export const PostsData = [
                 "Nunc ultrices gravida metus in pulvinar. Etiam eu scelerisque felis. Nulla erat sapien, molestie id mattis vitae, sagittis eu leo. Duis viverra eget justo eget condimentum. In ut enim consectetur, hendrerit elit at, auctor mi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Curabitur pharetra tortor ornare, ullamcorper arcu quis, tincidunt massa.",
                 "Sed molestie mi ipsum, et lacinia mi aliquet sit amet. Integer pretium a ipsum vitae mollis. Phasellus blandit nisl vel felis fringilla rutrum. Quisque imperdiet lorem elit, et ultricies arcu vehicula fringilla. Maecenas ultrices dolor vitae neque molestie mollis. Cras egestas lorem eu turpis ornare imperdiet. Cras iaculis molestie euismod. Quisque justo leo, pharetra et justo vitae, tempor pulvinar sem. Donec vitae nisl sit amet libero fermentum volutpat. Donec laoreet mauris nec odio lobortis, maximus eleifend arcu mollis. Fusce mauris eros, ullamcorper in dictum at, malesuada nec ipsum. Proin odio ipsum, auctor vitae porttitor lacinia, porttitor eget mi. Maecenas suscipit ut nibh quis imperdiet. Vivamus dictum ligula at gravida tempus. Aenean lacus velit, venenatis in ultrices tempus, tincidunt eu purus."
             ],
-        }
+        },
+        highlighted: true
     }
 ]
